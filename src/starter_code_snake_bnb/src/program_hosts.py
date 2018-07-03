02db25e80922ba1b834fb254b46e13c4052d9265
StevenCHowell <StevenCHowell@users.noreply.github.com>
2018-07-03 17:54:08 -0400

    begin updating availability

--- src/starter_code_snake_bnb/src/program_hosts.py
+++ src/starter_code_snake_bnb/src/program_hosts.py
@@ -113,16 +113,42 @@ def list_cages(suppress_header=False):
 
     cages = svc.find_cages_for_user(state.active_account)
     print(f'You have {len(cages)} cages.')
-    for c in cages:
-        print(f' * {c.name} is {c.square_meters} meters.')
+    for idx, c in enumerate(cages):
+        print(f' {idx+1}. {c.name} is {c.square_meters} meters.')
+        for b in c.bookings:
+            print('      * Booking {}, {} days, booked? {}'.format(
+                b.check_in_date,
+                (b.check_out_date - b.check_in_date).days,
+                'YES' if b.booked_date is not None else 'no'
+            ))
 
 
 def update_availability():
     print(' ****************** Add available date **************** ')
 
-    # TODO: Require an account
-    # TODO: list cages
-    # TODO: Choose cage
+    if not state.active_account:
+        error_msg('You must log in before updating cage availability.')
+        return
+
+    list_cages(suppress_header=True)
+
+    cage_number = input('Enter cage number: ').strip()
+    if not cage_number:
+        error_msg('Cancelled')
+        print()
+        return
+    cage_number = int(cage_number)
+
+    cages = svc.find_cages_for_user(state.active_account)
+    selected_cage = cages[cage_number - 1]
+
+    start_date = parser.parse(
+        input('Enter available date [yyyy-mm-dd]: ')
+    )
+    days = int(input('How many days is this block of time? '))
+
+    svc.add_available_date()
+
     # TODO: Set dates, save to DB.
 
     print(" -------- NOT IMPLEMENTED -------- ")
